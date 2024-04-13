577c14f93d527f1ccb71963435a3e22092031c15
Micholn <mdiagbare@gmail.com>
2024-04-13 23:46:12 +0100

    did the main routing

--- authrouter.js
+++ authrouter.js
@@ -2,5 +2,5 @@ var express = require('express');
 var authrouter = express.Router();
 
 authrouter.get('/login', function (req, res) {
-    res.render('Auth/auth_login')
+    res.render('Auth/auth_login');
 })
