051c092413390f00498a3aaae3a60342de997275
Micholn <mdiagbare@gmail.com>
2024-04-13 23:39:21 +0100

    started the server side of things

--- index.js
+++ index.js
@@ -3,30 +3,3 @@ var app = require('express');
 var express = require('express');
 var path = require('path');
 var http = require('http')
-
-
-// <!doctype html>
-// <html lang="en">
-// <head>
-//     <meta  charset="utf-8"/>
-//     <meta name="viewport" content="width=device-width, initial-scale=1" />
-//     <title>Random Quotes</title>
-//     <link rel="stylesheet" href=style.css>
-// </head>
-
-// <body>
-
-//     <div id="box" class="container">
-//         <div id="air" class="card">
-//             <h3>Advice Slip</h3>
-//             <p class="advice"></p>
-//             <br />
-//             <a id="get-advice">Get more advice</a>
-//         </div>
-//     </div>
-
-
-// <script src="index.js"></script>
-// </body>
-
-// </html>
