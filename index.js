64388411e72db36848ae77caabe40b5b80857a6b
Micholn <mdiagbare@gmail.com>
2024-04-14 00:08:47 +0100

    added the skeletons of the server

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ var app = require('express');
 var express = require('express');
 var path = require('path');
 var http = require('http').Server(app);
-var validator = require('express-vali
+var validator = require('express-validator');
