d6d3f757a6c5fa25877d7ca5838bb2d09cd68c13
Micholn <mdiagbare#gmail.com>
2024-10-25 16:42:20 +0100

    layering the indexes routes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var app = require('express');
+var app = require('express')();
 
 var express = require('express');
 var path = require('path');
@@ -7,4 +7,58 @@ var validator = require('express-validator');
 
 
 // import controller 
-var AuthController = require('./controllers/')
+var AuthController = require('./controllers/AuthController');
+
+//import Router file 
+var pageRouter = require('./routers/route');
+
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var flash = require('connect-flash');
+var i18n = require("i18n-express");
+app.use(bodyParser.json());
+var urlencodeParser = bodyParser.urlencoded({ extended: true });
+
+app.use(session({
+    key: 'user_sid',
+    secret: 'somerandomstuffs',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        expires: 1200000
+    }
+}));
+
+app.use(session({ resave: false, saveUninitialized: true, secret: 'nodedemo' }));
+app.use(flash());
+app.use(i18n({
+    translationPath: path.join(__dirname, 'i18n'),
+    siteLangs: ["es", "en", "ru", "it", "fr"],
+    textsVarName: 'translation'
+}));
+
+app.use('/public', express.static('public'));
+
+app.get('/layouts/', function(req, res1) {
+    res.render('view');
+});
+
+//apply controller
+AuthController(app);
+
+//For set layouts of html view 
+var expressLayouts = require('express-ejs-layouts');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+
+//Define All Route
+pageRouter(app);
+
+app.get('/', function(req, res) {
+    res.redirect('/');
+});
+
+http.listen(8000, function () {
+    console.log('listening on *: 8000');
+});
